279ce46d79833e06a8b2d1f96090698e13137501
Nishant Kumar <johndoe@email.com>
2024-09-27 11:51:59 +0530

    pivot index

--- pivot_index/main.go
+++ pivot_index/main.go
@@ -1,11 +1,19 @@
+//https://leetcode.com/problems/find-pivot-index/description/
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
 package main
 
 import "fmt"
 
 var pivotAray = []int{1, 7, 3, 6, 5, 6}
+
 // var pivotAray = []int{1,2,3}
 // var pivotAray = []int{2,1,-1}
-
 
 func main() {
 	totalSum := 0
@@ -17,7 +25,7 @@ func main() {
 
 	//now we have to iterate over the array
 	//sum the elements one by one into beforePivotSum
-	// rightArray = totalSum - beforePivotSum 
+	// rightArray = totalSum - beforePivotSum
 	// and to skip the last element substract it from above expression
 	// rightArray = totalSum - beforePivotSum - pivotAray[i]
 
